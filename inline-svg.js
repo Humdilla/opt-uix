f33ec7ec4931e47f1df2ed5be61bce3a25c5e478
Humdilla <Humdilla94@yahoo.com>
2017-02-13 10:33:02 -0500

    upload

--- inline-svg.js
+++ inline-svg.js
@@ -1,25 +1,90 @@
+'use strict';
 var Path = require('path');
-var FileSystem = require('fs');
+var fs = require('fs');
 var Minimist = require('minimist');
-var xml2js = require('xml2js');
+var htmlparser = require('htmlparser2');
+var util = require('util');
 
-function inlineSVG(path){
-  
+var SELFCLOSERS = [
+  'area',
+  'base',
+  'br',
+  'col',
+  'command',
+  'embed',
+  'hr',
+  'img',
+  'input',
+  'keygen',
+  'link',
+  'meta',
+  'param',
+  'source',
+  'track',
+  'wbr'
+];
+
+function inlineSVG(path, dest){
+  var buff = '';
+  var parser = new htmlparser.Parser({
+    onprocessinginstruction: function(instruction){
+      buff += '<'+instruction+'>';
+    },
+    onopentag: function(name, attribs){
+      if(name == 'img' && attribs.src.endsWith('.svg')){
+        buff += fs.readFileSync(attribs.src, 'utf8');
+      }
+      else {
+        buff += '<'+name;
+        for(let a in attribs){
+          buff += ' '+a+'="'+attribs[a]+'"';
+        }
+        buff += '>';
+      }
+    },
+    ontext: function(text){
+      buff += text;
+    },
+    onclosetag: function(name){
+      if(SELFCLOSERS.indexOf(name) != -1){
+        //noop
+      }
+      else {
+        buff += '</'+name+'>';
+      }
+    },
+    oncomment: function(comment){
+      buff += '<!--'+comment+'-->';
+    },
+    onerror: function(error){
+      console.log(error);
+    },
+    onend: function(){
+      if
+    }
+  });
+  fs.createReadStream(path, 'utf8').pipe(parser);
 }
 
 if(require.main === module){
-  var buff = '';
-  process.stdin.on('data', function(chunk){
-    buff += chunk;
-  })
-  process.stdin.on('end', function(){
-    var files = buff.split(' ');
-    var argv = Minimist(process.argv.slice(2));
-    console.log(argv);
-    files.forEach(function(path){
-      console.log(path);
+  var argv = Minimist(process.argv.slice(2));
+  if(argv._.length == 0) {  
+    var buff = '';
+    process.stdin.on('data', function(chunk){
+      buff += chunk;
+    })
+    process.stdin.on('end', function(){
+      var files = buff.split(' ');
+      files.forEach(function(path){
+        inlineSVG(path);
+      });
     });
-  });
+  }
+  else {
+    argv._.forEach(function(path){
+      inlineSVG(path);
+    });
+  }
 }
 else
   module.exports = inlineSVG;
