5127d0fbd58bf4f09c08eff42b1b2c2f85363d33
Humdilla <humdilla94@yahoo.com>
2017-02-16 22:25:22 -0500

    upload

--- inline-svg.js
+++ inline-svg.js
@@ -25,6 +25,25 @@ var SELFCLOSERS = [
   'wbr'
 ];
 
+/**
+ * Switch cwd to the given path, call the given function, and return to the
+ * path you were at previously.
+ * @param {String} path  Path to change to.
+ * @param {Function} cb  Function to call after changing directories. Takes
+ * no arguments.
+ */
+function doIn(path, cb){
+  var oldPath = process.cwd();
+  process.chdir(path);
+  cb();
+  process.chdir(oldPath);
+};
+
+/**
+ * Inlines the SVG sources of given HTML file.
+ * @param {String} path  Path to source HTML file.
+ * @return {Stream}      ReadStream with the output html file as the source.
+ */
 function inlineSVG(path){
   var source = fs.createReadStream(path, 'utf8');
   var dest = new stream.PassThrough();
@@ -33,8 +52,27 @@ function inlineSVG(path){
       dest.write('<'+instruction+'>', 'utf8');
     },
     onopentag: function(name, attribs){
+      /* Write external svg contents to output */
       if(name == 'img' && attribs.src.endsWith('.svg')){
-        dest.write(fs.readFileSync(attribs.src, 'utf8'));
+        doIn(Path.dirname(path), function(){
+          dest.write(fs.readFileSync(attribs.src, 'utf8'));
+        });
+      }
+      /* Write external css contents to output */
+      else if(name == 'link' && attribs.href.endsWith('.css')){
+        doIn(Path.dirname(path), function(){
+          dest.write('<style>'+fs.readFileSync(attribs.href, 'utf8')
+          /* Resolve relative paths found in css */
+          .replace(/url\((["']?(.*?)["']?)\)/gi, function(match, p1, p2){
+            doIn(Path.dirname(attribs.href), function(){
+              console.log(p2);
+              console.log(Path.dirname(path));
+              console.log(Path.relative(Path.dirname(path), p2));
+            });
+            return match;
+          })
+          +'</style>');
+        });
       }
       else {
         dest.write('<'+name);
@@ -67,24 +105,27 @@ function inlineSVG(path){
   });
   source.pipe(parser);
   return dest;
-}
+};
 
 if(require.main === module){
   var argv = Minimist(process.argv.slice(2));
   var dest;
   if(!argv.o){
-    console.log('Correct usage: node inline-svg [input file] -o [output file]');
-    process.exit(1);
+    dest = process.stdout;
   }
   else
     dest = fs.createWriteStream(argv.o);
   if(argv._.length == 0) {
-    /* We can assume our input is being piped from stdin */
+    /* If minimist finds no args we can assume our input is being piped from stdin */
     var buff = '';
     process.stdin.on('data', function(chunk){
       buff += chunk;
     });
     process.stdin.on('end', function(){
+      if(!buff){
+        console.log('Please specify an input file like so: node inline-svg.js [input_file]');
+        process.exit(1);
+      }
       inlineSVG(buff).pipe(dest);
     });
   }
